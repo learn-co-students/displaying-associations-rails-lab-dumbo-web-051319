429b7c8b52453214012eb95ef5cd7b98ba1fb57b
Daniela Sandoval <daniela.sandoval820@gmail.com>
2019-06-11 16:49:19 -0400

    able to show associations between 2 modes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# artists #
+Artist.create(name: "Daniela")
+Artist.create(name: "Mochi")
+Artist.create(name: "Lychee")
+
+#songs#
+Song.create(title: "I love cats", artist_id: 1)
+Song.create(title: "Food is Love", artist_id: 3)
+Song.create(title: "The Sun", artist_id: 2)
+Song.create(title: "Purrfect Food", artist_id: 3)
+Song.create(title: "Memow", artist_id: 2)
+Song.create(title: "FURRY MEOW", artist_id: 3)
